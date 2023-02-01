47acf3ab24c5191b4b1bfee65a75ddd4bfc01782
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-02-01 18:06:09 +0600

    update

---
+++ main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    another_function2()
+}
+
+fn another_function2() {
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
